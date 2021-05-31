da2b0295e3cf0259766aead37ac89de756f7d910
c-ferraz <cs.ferraz@outlook.com>
2021-05-31 20:57:31 -0300

    backend v1

---
+++ core/static/js/footer.js
@@ -0,0 +1,73 @@
+function footer() {
+    const footer = document.getElementById("footer-main")  
+    console.log('tezte')  
+    footer.innerHTML = `
+    
+                <div class="footer-content">
+                    <div class="logo-footer-copyright">
+                        <div class="logo-footer">
+                            <a href="../html/home.html">
+                                <img src="https://github.com/DevsOfWar/ArmazemDaCriatividade/blob/main/img-logo-paleta-cores-icons/Logo_ACC_Mono_Preta.png?raw=true" height="100%" width="130px" alt="logo-armazem">
+                            </a>
+                        </div>
+                        <div class="copyright">
+                            <p>© Devs of War. All rights reserved</p>
+                        </div>
+                    </div>                    
+                    <div class="social-medias">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <div class="social-medias-icon">
+                                        <img src="https://github.com/DevsOfWar/ArmazemDaCriatividade/blob/main/img-logo-paleta-cores-icons/redessociais/logo-do-facebook.png?raw=true" width="100%" height="100%" alt="">
+                                    </div>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#">
+                                    <div class="social-medias-icon">
+                                        <img src="https://raw.githubusercontent.com/DevsOfWar/ArmazemDaCriatividade/main/img-logo-paleta-cores-icons/redessociais/instagram.png" width="100%" height="100%" alt="">
+                                    </div>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#">
+                                    <div class="social-medias-icon">
+                                        <img src="https://github.com/DevsOfWar/ArmazemDaCriatividade/blob/main/img-logo-paleta-cores-icons/redessociais/logotipo-social-do-twitter.png?raw=true" width="100%" height="100%" alt="">
+                                    </div>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#">
+                                    <div class="social-medias-icon">
+                                        <img src="https://github.com/DevsOfWar/ArmazemDaCriatividade/blob/main/img-logo-paleta-cores-icons/redessociais/logotipo-do-youtube.png?raw=true" width="100%" height="100%" alt="">
+                                    </div>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#">
+                                    <div class="social-medias-icon">
+                                        <img src="https://github.com/DevsOfWar/ArmazemDaCriatividade/blob/main/img-logo-paleta-cores-icons/redessociais/linkedin.png?raw=true" width="100%" height="100%" alt="">
+                                    </div>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#">
+                                    <div class="social-medias-icon">
+                                        <img src="https://github.com/DevsOfWar/ArmazemDaCriatividade/blob/main/img-logo-paleta-cores-icons/redessociais/whatsapp.png?raw=true" width="100%" height="100%" alt="">
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            
+    `
+
+    
+}
